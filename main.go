c2d43451319fdcd27f9f9fc9098cc373709a6adc
100lvlmaster <navinkodag@gmail.com>
2021-08-31 00:24:19 +0530

    update: middleware again

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"github.com/100lvlmaster/5-chan-go/database"
 	"github.com/100lvlmaster/5-chan-go/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	app := fiber.New()
+	godotenv.Load()
 	app.Use(cors.New())
+	app.Use(cache.New())
 	database.DBConn = database.InitDb()
 	routes.RoutesInit(app)
 	app.Listen(":8080")
---
+++ middleware/middleware.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+/// Limit post requests to the official client
+func CustomeMiddleware() func(*fiber.Ctx) error {
+
+	return func(c *fiber.Ctx) error {
+		if c.Method() == "POST" {
+			apiKey, envExists := os.LookupEnv("API_KEY")
+			if !envExists {
+				return c.Status(500).SendString("Could not fetch env vars, call 100lvlmaster")
+			}
+			key := c.Get("Authorization")
+			if apiKey != key {
+				return c.Status(400).SendString("Your keys are incorrect peasant ✨✨")
+			}
+		}
+		return c.Next()
+	}
+}
